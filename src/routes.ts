8c225f5c0d5f4b6de8259ff224868021986a3b04
Matheus Pereira <matheusp.dasilva@hotmail.com>
2024-11-28 07:07:16 -0300

    Create Route and server

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import { CreateBankController } from './controllers/Bank/CreateBankController';
+import { GetBankController } from './controllers/Bank/GetBankController';
+import { DelBankController } from './controllers/Bank/DelBankController';
 
 const router = Router();
 
 // Rotas Bank
-router.post('/bank', new CreateBankController().handle)
+router.post('/bank', new CreateBankController().handle);
+router.get('/bank', new GetBankController().handle);
+router.delete('/bank', new DelBankController().handle);
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ const app = express();
 const port = 3333;
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 app.use(cors());
 
 app.use(router);
